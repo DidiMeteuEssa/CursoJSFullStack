a7eed3071cef6b08a84e795051ff5c63de8ca58c
Diego Felippe <diegolppe@gmail.com>
2025-08-17 04:19:46 -0300

    atualizando aulas

---
+++ React/exercicio-1/src/App.jsx
@@ -0,0 +1,27 @@
+export default function App() {
+  return (
+    <div className="">
+      <img src="/react.png" alt="" />
+      <h1>React</h1>
+      <p>A bibliotca para interfaces de usuario web e nativas.</p>
+      <button>Aprenda React</button>
+      <button>Referência da API</button>
+      <hr />
+      <h1>Crie interfacess de usuário de componentes</h1>
+      <p>Crie interfaces de usuário a partir de predaços individuais chamandos componentes</p>
+      <hr />
+      <h1>Escreva componentes com código e marcaçao</h1>
+      <p>Componentes React sao funçoes JavaScript. A sintaxe de marcaçao é chamada de JSX. É uma extensao de sintaxe JavaScript poularizada no React</p>
+      <hr />
+      <h1>Próximos passos</h1>
+      <ul>
+        <li>Uso de dados dinâmicos no JSX</li>
+        <li>Criaçao de novos componentes</li>
+        <li>Estilizaçao de componentes</li>
+        <li>Reutulizacao de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos do JavaScript</li>
+      </ul>
+    </div>
+  )
+}
---
+++ React/primeira-pagona/index.jsx
@@ -0,0 +1,12 @@
+function PrimaryButton() {
+    return <button>Meu butaum</button>
+}
+
+function Container() {
+    return (
+        <div className="" >
+            <h1></h1>
+            <p></p>
+        </div >
+    )
+}
